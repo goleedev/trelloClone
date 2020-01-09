713ef05c4f975739b43bceb121d71696e6067e03
GO Lee <golee.dev@gmail.com>
2020-01-09 22:33:40 +0900

    Add trello list and card

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TrelloList from "./TrelloList";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello</h2>
+        <TrelloList title="test"/>
     </div>
   );
 }
