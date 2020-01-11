acb8012d0ede58316ccf844beea57b1d365a325a
GO Lee <golee.dev@gmail.com>
2020-01-11 21:00:29 +0900

    add button

--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -9,11 +9,29 @@ const initialState = [
             },
             {
                 id : 1,
-                text : "We used a mix between material UI React and styled compone"
+                text : "We used a mix between material UI React and styled components"
+            }
+        ]
+    },
+    {
+        title : "This Episode",
+        id : 1,
+        cards : [
+            {
+                id : 0,
+                text : "We will create our first reducer"
+            },
+            {
+                id : 1,
+                text : "and render many cards on our list with static data "
+            },
+            {
+                id : 2,
+                text : "we will also make some little changes I forgot in the last episode"
             }
         ]
     }
-]
+];
 
 const listsReducer = (state = initialState, action) => {
     switch (action.type) {
